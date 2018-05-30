374129ad4c309849e763835ac92078d8a8bbc9fe
prdn <paolo.ardoino@gmail.com>
2018-05-30 19:57:37 +0000

    command-line args for ballot

--- bin/antani-ballot.js
+++ bin/antani-ballot.js
@@ -10,16 +10,16 @@ var cmd = argv._[0]
 
 switch (cmd) {
   case 'init': return init(argv._[1])
-  case 'finalize': return finalize(argv._[1], argv._[2], argv._[3])
-  case 'cast': return cast(argv._[1], argv._[2], argv._[3], argv._[4])
-  case 'tally': return tally(argv._[1], argv._[2], argv._[3])
+  case 'finalize': return finalize(argv._[1], argv._[2], argv._[3], argv._[4])
+  case 'cast': return cast(argv._[1], argv._[2], argv._[3], argv._[4], argv._[5])
+  case 'tally': return tally(argv._[1], argv._[2], argv._[3], argv._[4])
   default:
     console.error(`Usage: antani-ballot <cmd> [options]
 
   antani-ballot init <key-pair>
-  antani-ballot finalize <key-pair> <ballot-file> <tree-file>
-  antani-ballot cast <key-pair> <ballot-file> <tree-file> <vote>
-  antani-ballot tally <key-pair> <ballot-file> <tree-file>
+  antani-ballot finalize <key-pair> <ballot-file> <tree-file> <candidates>
+  antani-ballot cast <key-pair> <ballot-file> <tree-file> <vote> <candidates>
+  antani-ballot tally <key-pair> <ballot-file> <tree-file> <candidates>
 `)
   process.exit(1)
 }
@@ -28,10 +28,10 @@ function init (keyPath) {
   fs.writeFileSync(path.join(process.cwd(), keyPath), JSON.stringify(antani.ballot.keygen()))
 }
 
-function cast (keyPath, ballotPath, treePath, voteStr) {
+function cast (keyPath, ballotPath, treePath, voteStr, candidates) {
   var t = antani.tree(path.join(process.cwd(), treePath))
   var keypair = JSON.parse(fs.readFileSync(path.join(process.cwd(), keyPath), 'utf8'))
-  var candidates = ['pindis', 'rød grød med fløde', 'koldskål']
+  candidates = (candidates || '').split(';')
   var b = antani.ballot(path.join(process.cwd(), ballotPath), t, {keypair, candidates})
 
   var vote = JSON.parse(voteStr)
@@ -39,19 +39,19 @@ function cast (keyPath, ballotPath, treePath, voteStr) {
   b.push(vote, console.log)
 }
 
-function finalize (keyPath, ballotPath, treePath) {
+function finalize (keyPath, ballotPath, treePath, candidates) {
   var t = antani.tree(path.join(process.cwd(), treePath))
   var keypair = JSON.parse(fs.readFileSync(path.join(process.cwd(), keyPath), 'utf8'))
-  var candidates = ['pindis', 'rød grød med fløde', 'koldskål']
+  candidates = (candidates || '').split(';')
   var b = antani.ballot(path.join(process.cwd(), ballotPath), t, {keypair, candidates})
 
   b.finalize(console.log)
 }
 
-function tally (keyPath, ballotPath, treePath) {
+function tally (keyPath, ballotPath, treePath, candidates) {
   var t = antani.tree(path.join(process.cwd(), treePath))
   var keypair = JSON.parse(fs.readFileSync(path.join(process.cwd(), keyPath), 'utf8'))
-  var candidates = ['pindis', 'rød grød med fløde', 'koldskål']
+  candidates = (candidates || '').split(';')
   var b = antani.ballot(path.join(process.cwd(), ballotPath), t, {keypair, candidates})
 
   b.tally(console.log)
